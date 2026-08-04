54be7c801de533e6633aa2d7148736f78de5ae68
Lukian Yuzvenko <lukayuzvenko@gmail.com>
2026-08-04 15:10:03 -0400

    Moving grid character

--- scripts/_start.js
+++ scripts/_start.js
@@ -4,6 +4,20 @@ kaboom({
     height: 720,
 });
 
-const UNIT = 25;
+const UNIT = 30;
+const UNIT_INVERSE = 1/UNIT;
+const UNIT_HALF = UNIT/2;
 
 loadBean();
+
+function toGridPos(v) {
+    return v.scale(UNIT_INVERSE)
+}
+
+function fromGridPos(v) {
+    return v.scale(UNIT)
+}
+
+function roundVec(v) {
+    return vec2(Math.round(v.x), Math.round(v.y))
+}
--- scripts/game.js
+++ scripts/game.js
@@ -13,18 +13,40 @@ add([
 
 const playerData = {
     pos: vec2(0,0),
+    finePos: vec2(0,0),
+    direction: vec2(0,0),
+    speed: 10,
 }
 
 onUpdate(() => {
-    playerData.pos = playerData.pos.add(3 * dt())
+    if (isMouseDown()) {
+        let playerMouseDiff = mousePos().sub(
+            toScreen(fromGridPos(playerData.pos))
+        );
+
+        playerData.velocity = playerMouseDiff.unit();
+    } else {
+        playerData.velocity = vec2(0);
+    }
+
+    playerData.finePos = playerData.finePos.add(
+        playerData.velocity.scale(dt() * playerData.speed)
+    );
+    playerData.pos = roundVec(playerData.finePos);
 })
 
 onDraw(() => {
     drawRect({
         width: UNIT,
         height: UNIT,
-        pos: playerData.pos.scale(UNIT),
+        pos: fromGridPos(playerData.pos.sub(0.5)),
         color: WHITE,
+    });
+    drawRect({
+        width: 2,
+        height: 2,
+        pos: vec2(0,0),
+        color: RED,
     });
 })
 
